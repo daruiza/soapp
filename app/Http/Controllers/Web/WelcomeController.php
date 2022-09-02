121d35a3355dd75259f2a01ae71ec41dd090ae30
david.Ruiz <david.ruiz@software.com>
2022-09-02 13:00:25 -0500

    Correccion de Web Routas

---
+++ app/Http/Controllers/Web/WelcomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        return 'Welcome';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', 'welcome');
+Route::get('/', 'WelcomeController@index');
