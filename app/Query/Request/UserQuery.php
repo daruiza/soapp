1085eb3944f4b8bbd42716dfc893862d48724145
david.Ruiz <david.ruiz@software.com>
2022-09-25 20:00:53 -0500

    Corrección Commerce Update

--- app/Query/Request/UserQuery.php
+++ app/Query/Request/UserQuery.php
@@ -105,7 +105,7 @@ class UserQuery implements IUserQuery
             try {
                 $user = User::findOrFail($id);
                 $request->validate([
-                    $this->name     => 'required|string|min:5|max:128',
+                    $this->name     => 'required|string|min:0|max:128',
                     $this->email    => 'required|string|max:128|email|', Rule::unique('users')->ignore($user->id),
                     $this->phone    => 'min:7|max:10'
                 ]);
