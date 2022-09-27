6d90103d08ad79f0a1cbaf742d43175d5d8e402c
david.Ruiz <david.ruiz@software.com>
2022-09-27 12:05:24 -0500

    Userquery phone validator

--- app/Query/Request/UserQuery.php
+++ app/Query/Request/UserQuery.php
@@ -107,7 +107,7 @@ class UserQuery implements IUserQuery
                 $request->validate([
                     $this->name     => 'required|string|min:0|max:128',
                     $this->email    => 'required|string|max:128|email|', Rule::unique('users')->ignore($user->id),
-                    $this->phone    => 'min:7|max:10'
+                    $this->phone    => 'min:0|max:10'
                 ]);
                 $user->name     = $request->name ?? $user->name;
                 $user->lastname = $request->lastname ?? $user->lastname;
