17ca594b4b3013ac854d234b3213d0b883b8b37b
daruiza <daruiza@gmail.com>
2023-07-12 21:14:48 -0500

    add new TrainingsstEevent Entity

---
+++ app/Model/Request/TrainingsstEvidence.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @OA\Schema(
+ *      title="Evidence",
+ *      description="Evidence body data",
+ *      type="object"
+ * )
+ */
+class TrainingsstEvidence
+{
+
+    /**
+     * @OA\Property(
+     *      title="name",
+     *      description="Name of the evidence",
+     *      example="FileName"
+     * )
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @OA\Property(
+     *      title="file",
+     *      description="File of the evidence",
+     *      example="FileHostingName"
+     * )
+     *
+     * @var string
+     */
+    public $file;
+
+    /**
+     * @OA\Property(
+     *      title="type",
+     *      description="Type File",
+     *      example="image/jpeg"
+     * )
+     *
+     * @var string
+     */
+    public $type;
+
+
+    /**
+     * @OA\Property(
+     *      title="approved",
+     *      description="approved of the evidence",
+     *      example=true
+     * )
+     *
+     * @var boolean
+     */
+    public $approved;
+
+
+    /**
+     * @OA\Property(
+     *      title="trainingsst_id",
+     *      description="trainingsst_id of the Evidence",
+     *      example=1
+     * )
+     *
+     * @var numeric
+     */
+    public $trainingsst_id;
+
+}
