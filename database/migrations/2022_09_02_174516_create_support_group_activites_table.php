430111c0e1625e52cdc78007c3c6340ae0eef9dd
daruiza <daruiza@gmail.com>
2024-03-16 10:02:42 -0500

    cambios con dumar

---
+++ database/migrations/2022_09_02_174516_create_support_group_activites_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupportGroupActivitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('support_group_activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('support_group', 128);
+            $table->date('date_meet')->nullable()->default(null);                        
+            $table->string('responsible')->nullable();     
+            $table->string('tasks_copas')->nullable();     
+            $table->unsignedBigInteger('report_id');
+            $table->foreign('report_id')
+                ->references('id')
+                ->on('reports')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('support_group_activities');
+    }
+}
