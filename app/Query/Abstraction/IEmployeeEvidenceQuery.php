09fb964c41856402e5ac01c4d500e3ec52193469
daruiza <daruiza@gmail.com>
2024-06-13 19:11:37 -0500

    get getAllByCommerceId complete

--- app/Query/Abstraction/IEmployeeEvidenceQuery.php
+++ app/Query/Abstraction/IEmployeeEvidenceQuery.php
@@ -5,7 +5,7 @@ namespace App\Query\Abstraction;
 use Illuminate\Http\Request;
 
 interface IEmployeeEvidenceQuery {
-    public function index(Request $request);
+    public function index(Request $request);    
     public function store(Request $request);
     public function update(Request $request, int $id);
     public function destroy(Int $id);
