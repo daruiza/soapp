7e5be294035f6751c234621c44c718d6177cc822
daruiza <daruiza@gmail.com>
2024-06-16 17:17:46 -0500

    configuarncón to soappsorage

--- database/seeds/EmployeesSeeder.php
+++ database/seeds/EmployeesSeeder.php
@@ -12,88 +12,88 @@ class EmployeesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('employees')->insert(array(
-            'identification_type' => 'Cédula Ciudadania',
-            'identification' => '1039450689',
-            'name' => 'Pedro',
-            'lastname' => 'Paramo',
-            'email' => 'paramo.pedro@gmail',
-            'birth_date' => '2000-01-01',
-            'phone' => '215478596',
-            'adress' => 'San Pedro',
-            'photo' => null,
-            'eps' => 'SURA',
-            'commerce_id'=> 1
-        ));
+        // DB::table('employees')->insert(array(
+        //     'identification_type' => 'Cédula Ciudadania',
+        //     'identification' => '1039450689',
+        //     'name' => 'Pedro',
+        //     'lastname' => 'Paramo',
+        //     'email' => 'paramo.pedro@gmail',
+        //     'birth_date' => '2000-01-01',
+        //     'phone' => '215478596',
+        //     'adress' => 'San Pedro',
+        //     'photo' => null,
+        //     'eps' => 'SURA',
+        //     'commerce_id'=> 1
+        // ));
 
-        DB::table('employees')->insert(array(
-            'identification_type' => 'Cédula Ciudadania',
-            'identification' => '1039450648',
-            'name' => 'Jose',
-            'lastname' => 'Saramago',
-            'email' => 'saramago.jose@gmail',
-            'birth_date' => '1990-01-01',
-            'phone' => '2154716',
-            'adress' => 'San Jose',
-            'photo' => null,
-            'eps' => 'SURA',
-            'commerce_id'=> 1
-        ));
+        // DB::table('employees')->insert(array(
+        //     'identification_type' => 'Cédula Ciudadania',
+        //     'identification' => '1039450648',
+        //     'name' => 'Jose',
+        //     'lastname' => 'Saramago',
+        //     'email' => 'saramago.jose@gmail',
+        //     'birth_date' => '1990-01-01',
+        //     'phone' => '2154716',
+        //     'adress' => 'San Jose',
+        //     'photo' => null,
+        //     'eps' => 'SURA',
+        //     'commerce_id'=> 1
+        // ));
 
-        DB::table('employees')->insert(array(
-            'identification_type' => 'Cédula Ciudadania',
-            'identification' => '1039785458',
-            'name' => 'Teo',
-            'lastname' => 'Rua',
-            'email' => 'rua.jose@gmail',
-            'birth_date' => '1990-01-01',
-            'phone' => '2154716',
-            'adress' => 'San Jose',
-            'photo' => null,
-            'eps' => 'SURA',
-            'commerce_id'=> 1
-        ));
+        // DB::table('employees')->insert(array(
+        //     'identification_type' => 'Cédula Ciudadania',
+        //     'identification' => '1039785458',
+        //     'name' => 'Teo',
+        //     'lastname' => 'Rua',
+        //     'email' => 'rua.jose@gmail',
+        //     'birth_date' => '1990-01-01',
+        //     'phone' => '2154716',
+        //     'adress' => 'San Jose',
+        //     'photo' => null,
+        //     'eps' => 'SURA',
+        //     'commerce_id'=> 1
+        // ));
 
-        DB::table('employees')->insert(array(
-            'identification_type' => 'Cédula Ciudadania',
-            'identification' => '1039783692',
-            'name' => 'Valentino',
-            'lastname' => 'Cervantes',
-            'email' => 'valentino.jose@gmail',
-            'birth_date' => '1990-01-01',
-            'phone' => '2154716',
-            'adress' => 'San Jose',
-            'photo' => null,
-            'eps' => 'SURA',
-            'commerce_id'=> 2
-        ));
+        // DB::table('employees')->insert(array(
+        //     'identification_type' => 'Cédula Ciudadania',
+        //     'identification' => '1039783692',
+        //     'name' => 'Valentino',
+        //     'lastname' => 'Cervantes',
+        //     'email' => 'valentino.jose@gmail',
+        //     'birth_date' => '1990-01-01',
+        //     'phone' => '2154716',
+        //     'adress' => 'San Jose',
+        //     'photo' => null,
+        //     'eps' => 'SURA',
+        //     'commerce_id'=> 2
+        // ));
 
-        DB::table('employees')->insert(array(
-            'identification_type' => 'Cédula Ciudadania',
-            'identification' => '1039783678',
-            'name' => 'Simba',
-            'lastname' => 'León',
-            'email' => 'leon.simba@gmail',
-            'birth_date' => '1989-08-04',
-            'phone' => '2154716',
-            'adress' => 'Cr 86c # 53c-41',
-            'photo' => null,
-            'eps' => 'SURA',
-            'commerce_id'=> 2
-        ));
+        // DB::table('employees')->insert(array(
+        //     'identification_type' => 'Cédula Ciudadania',
+        //     'identification' => '1039783678',
+        //     'name' => 'Simba',
+        //     'lastname' => 'León',
+        //     'email' => 'leon.simba@gmail',
+        //     'birth_date' => '1989-08-04',
+        //     'phone' => '2154716',
+        //     'adress' => 'Cr 86c # 53c-41',
+        //     'photo' => null,
+        //     'eps' => 'SURA',
+        //     'commerce_id'=> 2
+        // ));
 
-        DB::table('employees')->insert(array(
-            'identification_type' => 'Pasaporte',
-            'identification' => '1039783784',
-            'name' => 'Mufasha',
-            'lastname' => 'León',
-            'email' => 'mufasha.simba@gmail',
-            'birth_date' => '1973-02-27',
-            'phone' => '2154716',
-            'adress' => 'Cl60 # 75 -150',
-            'photo' => null,
-            'eps' => 'SURA',
-            'commerce_id'=> 2
-        ));
+        // DB::table('employees')->insert(array(
+        //     'identification_type' => 'Pasaporte',
+        //     'identification' => '1039783784',
+        //     'name' => 'Mufasha',
+        //     'lastname' => 'León',
+        //     'email' => 'mufasha.simba@gmail',
+        //     'birth_date' => '1973-02-27',
+        //     'phone' => '2154716',
+        //     'adress' => 'Cl60 # 75 -150',
+        //     'photo' => null,
+        //     'eps' => 'SURA',
+        //     'commerce_id'=> 2
+        // ));
     }
 }
--- database/seeds/ReportsSeeder.php
+++ database/seeds/ReportsSeeder.php
@@ -12,44 +12,44 @@ class ReportsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('reports')->insert(array(            
-            'project'  => 'SST',
-            'progress'  => 21,
-            'responsible'  => 'Agente01',
-            'email_responsible'  => 'agente01@gmail.com',
-            'phone_responsible'  => '12345678',
-            'date'  => '2023-01-01',
-            'commerce_id'  => '1',
-        ));
+        // DB::table('reports')->insert(array(            
+        //     'project'  => 'SST',
+        //     'progress'  => 21,
+        //     'responsible'  => 'Agente01',
+        //     'email_responsible'  => 'agente01@gmail.com',
+        //     'phone_responsible'  => '12345678',
+        //     'date'  => '2023-01-01',
+        //     'commerce_id'  => '1',
+        // ));
 
-        DB::table('reports')->insert(array(
-            'project'  => 'CAIDAD',
-            'progress'  => 74,
-            'responsible'  => 'Agente01',
-            'email_responsible'  => 'agente01@gmail.com',
-            'phone_responsible'  => '3194052663',
-            'date'  => '2023-01-01',
-            'commerce_id'  => '2',
-        ));
+        // DB::table('reports')->insert(array(
+        //     'project'  => 'CAIDAD',
+        //     'progress'  => 74,
+        //     'responsible'  => 'Agente01',
+        //     'email_responsible'  => 'agente01@gmail.com',
+        //     'phone_responsible'  => '3194052663',
+        //     'date'  => '2023-01-01',
+        //     'commerce_id'  => '2',
+        // ));
 
-        DB::table('reports')->insert(array(
-            'project'  => 'SST',
-            'progress'  => 49,
-            'responsible'  => 'Agente01',
-            'email_responsible'  => 'agente01@gmail.com',
-            'phone_responsible'  => '3194056987',
-            'date'  => '2023-02-01',
-            'commerce_id'  => '2',
-        ));
+        // DB::table('reports')->insert(array(
+        //     'project'  => 'SST',
+        //     'progress'  => 49,
+        //     'responsible'  => 'Agente01',
+        //     'email_responsible'  => 'agente01@gmail.com',
+        //     'phone_responsible'  => '3194056987',
+        //     'date'  => '2023-02-01',
+        //     'commerce_id'  => '2',
+        // ));
 
-        DB::table('reports')->insert(array(
-            'project'  => 'MEDIOAMBIENTE',
-            'progress'  => 16,
-            'responsible'  => 'Agente',
-            'email_responsible'  => 'agente@gmail.com',
-            'phone_responsible'  => '12345678',
-            'date'  => '2023-04-01',
-            'commerce_id'  => '3',
-        ));
+        // DB::table('reports')->insert(array(
+        //     'project'  => 'MEDIOAMBIENTE',
+        //     'progress'  => 16,
+        //     'responsible'  => 'Agente',
+        //     'email_responsible'  => 'agente@gmail.com',
+        //     'phone_responsible'  => '12345678',
+        //     'date'  => '2023-04-01',
+        //     'commerce_id'  => '3',
+        // ));
     }
 }
