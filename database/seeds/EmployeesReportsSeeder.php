84aca26f51a329885654005a7b91723227bfa451
daruiza <daruiza@gmail.com>
2024-06-17 08:58:45 -0500

    coment de empleyee report

--- database/seeds/EmployeesReportsSeeder.php
+++ database/seeds/EmployeesReportsSeeder.php
@@ -12,34 +12,34 @@ class EmployeesReportsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('employee_report')->insert(array(
-            'employee_id' => '1',
-            'report_id' => '1'
-        ));
+        // DB::table('employee_report')->insert(array(
+        //     'employee_id' => '1',
+        //     'report_id' => '1'
+        // ));
 
-        DB::table('employee_report')->insert(array(
-            'employee_id' => '4',
-            'report_id' => '2'
-        ));
+        // DB::table('employee_report')->insert(array(
+        //     'employee_id' => '4',
+        //     'report_id' => '2'
+        // ));
 
-        DB::table('employee_report')->insert(array(
-            'employee_id' => '2',
-            'report_id' => '1'
-        ));
+        // DB::table('employee_report')->insert(array(
+        //     'employee_id' => '2',
+        //     'report_id' => '1'
+        // ));
 
-        DB::table('employee_report')->insert(array(
-            'employee_id' => '3',
-            'report_id' => '1'
-        ));
+        // DB::table('employee_report')->insert(array(
+        //     'employee_id' => '3',
+        //     'report_id' => '1'
+        // ));
 
-        DB::table('employee_report')->insert(array(
-            'employee_id' => '5',
-            'report_id' => '3'
-        ));
+        // DB::table('employee_report')->insert(array(
+        //     'employee_id' => '5',
+        //     'report_id' => '3'
+        // ));
 
-        DB::table('employee_report')->insert(array(
-            'employee_id' => '6',
-            'report_id' => '3'
-        ));
+        // DB::table('employee_report')->insert(array(
+        //     'employee_id' => '6',
+        //     'report_id' => '3'
+        // ));
     }
 }
